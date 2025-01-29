ae983844d76fd0cd718e09f018d128a9bac1513e
mateusrs1 <mateusrshu1@gmail.com>
2025-01-29 18:09:49 -0300

    v7 sql

--- nutribox.sql
+++ nutribox.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 21/08/2024 às 23:35
+-- Tempo de geração: 14/09/2024 às 21:55
 -- Versão do servidor: 10.4.32-MariaDB
 -- Versão do PHP: 8.0.30
 
@@ -30,31 +30,9 @@ SET time_zone = "+00:00";
 CREATE TABLE `tb_carrinho` (
   `id` int(11) NOT NULL,
   `usuario_id` int(11) NOT NULL,
-  `marmita_id` int(11) NOT NULL,
+  `produto_id` int(11) NOT NULL,
   `quantidade` int(11) NOT NULL DEFAULT 1
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
--- --------------------------------------------------------
-
---
--- Estrutura para tabela `tb_marmitas`
---
-
-CREATE TABLE `tb_marmitas` (
-  `id` int(11) NOT NULL,
-  `descricao` text NOT NULL,
-  `preco` int(11) NOT NULL,
-  `nome` varchar(255) NOT NULL,
-  `imagem` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Despejando dados para a tabela `tb_marmitas`
---
-
-INSERT INTO `tb_marmitas` (`id`, `descricao`, `preco`, `nome`, `imagem`) VALUES
-(3, '- Arroz <br> \n- Feijão <br>\n- Batata <br>\n- Salada <br>\n- Farofa <br>\n- Carne do Dia <br>', 16, 'Marmita P', '1723593270_52866d65-a282-4568-ad0d-08c526bec45f.webp'),
-(12, '- Arroz <br> \r\n- Feijão <br>\r\n- Batata <br>\r\n- Salada <br>\r\n- Farofa <br>\r\n- Carne do Dia <br>\r\n- Pure', 20, 'Marmita M', '1723756910_Dicas-para-uma-marmita.png');
 
 -- --------------------------------------------------------
 
@@ -77,13 +55,22 @@ CREATE TABLE `tb_pedidos` (
 INSERT INTO `tb_pedidos` (`id`, `usuario_id`, `status`, `data_criacao`, `valor_total`) VALUES
 (1, 29, 'PENDENTE', '2024-08-21 17:06:27', 20.00),
 (2, 29, 'ENTREGUE', '2024-08-21 17:06:34', 40.00),
-(3, 29, 'PENDENTE', '2024-08-21 17:07:09', 40.00),
+(3, 29, 'ENTREGUE', '2024-08-21 17:07:09', 40.00),
 (4, 29, 'PENDENTE', '2024-08-21 17:18:24', 80.00),
 (5, 29, 'ENTREGUE', '2024-08-21 17:32:58', 36.00),
 (6, 28, 'PENDENTE', '2024-08-21 17:41:31', 36.00),
-(7, 28, 'PENDENTE', '2024-08-21 17:51:10', 140.00),
-(8, 28, 'PENDENTE', '2024-08-21 17:51:22', 16.00),
-(9, 29, 'PENDENTE', '2024-08-21 18:33:02', 212.00);
+(7, 28, 'ENTREGUE', '2024-08-21 17:51:10', 140.00),
+(8, 28, 'EM PREPARO', '2024-08-21 17:51:22', 16.00),
+(9, 29, 'ENTREGUE', '2024-08-21 18:33:02', 212.00),
+(11, 29, 'PENDENTE', '2024-09-11 18:05:00', 20.00),
+(12, 29, 'PENDENTE', '2024-09-11 18:05:36', 40.00),
+(13, 28, 'ENTREGUE', '2024-09-11 18:09:05', 16.00),
+(14, 29, 'PENDENTE', '2024-09-11 18:20:29', 60.00),
+(15, 29, 'PENDENTE', '2024-09-11 18:20:55', 60.00),
+(16, 29, 'PENDENTE', '2024-09-11 18:20:57', 60.00),
+(17, 29, 'PENDENTE', '2024-09-11 18:21:00', 80.00),
+(18, 29, 'PENDENTE', '2024-09-11 18:21:35', 80.00),
+(19, 29, 'PENDENTE', '2024-09-12 22:05:38', 66.00);
 
 -- --------------------------------------------------------
 
@@ -94,7 +81,7 @@ INSERT INTO `tb_pedidos` (`id`, `usuario_id`, `status`, `data_criacao`, `valor_t
 CREATE TABLE `tb_pedido_itens` (
   `id` int(11) NOT NULL,
   `pedido_id` int(11) NOT NULL,
-  `marmita_id` int(11) NOT NULL,
+  `produto_id` int(11) NOT NULL,
   `quantidade` int(11) NOT NULL,
   `preco` decimal(10,2) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
@@ -103,7 +90,7 @@ CREATE TABLE `tb_pedido_itens` (
 -- Despejando dados para a tabela `tb_pedido_itens`
 --
 
-INSERT INTO `tb_pedido_itens` (`id`, `pedido_id`, `marmita_id`, `quantidade`, `preco`) VALUES
+INSERT INTO `tb_pedido_itens` (`id`, `pedido_id`, `produto_id`, `quantidade`, `preco`) VALUES
 (1, 3, 12, 2, 20.00),
 (2, 4, 12, 4, 20.00),
 (3, 5, 3, 1, 16.00),
@@ -113,7 +100,36 @@ INSERT INTO `tb_pedido_itens` (`id`, `pedido_id`, `marmita_id`, `quantidade`, `p
 (7, 7, 12, 7, 20.00),
 (8, 8, 3, 1, 16.00),
 (9, 9, 3, 7, 16.00),
-(10, 9, 12, 5, 20.00);
+(10, 9, 12, 5, 20.00),
+(13, 11, 12, 1, 20.00),
+(14, 12, 12, 2, 20.00),
+(15, 13, 3, 1, 16.00),
+(16, 18, 12, 4, 20.00),
+(17, 19, 12, 1, 20.00),
+(18, 19, 13, 3, 8.00);
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `tb_produtos`
+--
+
+CREATE TABLE `tb_produtos` (
+  `id` int(11) NOT NULL,
+  `descricao` text NOT NULL,
+  `preco` int(11) NOT NULL,
+  `nome` varchar(255) NOT NULL,
+  `imagem` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Despejando dados para a tabela `tb_produtos`
+--
+
+INSERT INTO `tb_produtos` (`id`, `descricao`, `preco`, `nome`, `imagem`) VALUES
+(3, '- Arroz <br> \n- Feijão <br>\n- Batata <br>\n- Salada <br>\n- Farofa <br>\n- Carne do Dia <br>', 16, 'Marmita P', '1723593270_52866d65-a282-4568-ad0d-08c526bec45f.webp'),
+(12, '- Arroz <br> \r\n- Feijão <br>\r\n- Batata <br>\r\n- Salada <br>\r\n- Farofa <br>\r\n- Carne do Dia <br>\r\n- Pure', 20, 'Marmita M', '1723756910_Dicas-para-uma-marmita.png'),
+(13, 'Coca docinha para você curtir sua marmita com sabor e qualidade', 8, 'Coca Top Zero', '1726089774_coca-cola-zero-600-ml.jpg');
 
 -- --------------------------------------------------------
 
@@ -123,13 +139,13 @@ INSERT INTO `tb_pedido_itens` (`id`, `pedido_id`, `marmita_id`, `quantidade`, `p
 
 CREATE TABLE `tb_user` (
   `ID` int(11) NOT NULL,
-  `nome` varchar(254) NOT NULL,
-  `img` text NOT NULL,
-  `cargo` varchar(12) NOT NULL,
-  `nivel` int(11) NOT NULL DEFAULT 1,
-  `xp` int(11) NOT NULL,
-  `email` varchar(200) NOT NULL,
-  `senha` varchar(16) NOT NULL
+  `nome` varchar(254) NOT NULL DEFAULT 'admin',
+  `img` text DEFAULT NULL,
+  `cargo` varchar(12) NOT NULL DEFAULT 'ADMIN',
+  `nivel` int(11) DEFAULT 1,
+  `xp` int(11) NOT NULL DEFAULT 1,
+  `email` varchar(200) NOT NULL DEFAULT 'admin@gmail.com',
+  `senha` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
@@ -137,8 +153,8 @@ CREATE TABLE `tb_user` (
 --
 
 INSERT INTO `tb_user` (`ID`, `nome`, `img`, `cargo`, `nivel`, `xp`, `email`, `senha`) VALUES
-(28, 'admin', 'uploads/IMG-20240804-WA0054.jpg', 'USUARIO', 1, 58, 'wesley@gmail.com', '1234567'),
-(29, 'admin', 'uploads/Default_Create_a_logo_for_a_web_development_agency_that_convey_0.jpg', 'COZINHEIRO', 2, 111, 'conradtsamuel@gmail.com', '1234567');
+(28, 'Wesley', 'uploads/IMG-20240804-WA0054.jpg', 'USER', 1, 63, 'wesley@gmail.com', '1234567'),
+(29, 'admin', 'uploads/Default_Create_a_logo_for_a_web_development_agency_that_convey_0.jpg', 'ADMIN', 3, 261, 'conradtsamuel@gmail.com', '1234567');
 
 --
 -- Índices para tabelas despejadas
@@ -150,13 +166,7 @@ INSERT INTO `tb_user` (`ID`, `nome`, `img`, `cargo`, `nivel`, `xp`, `email`, `se
 ALTER TABLE `tb_carrinho`
   ADD PRIMARY KEY (`id`),
   ADD KEY `usuario_id` (`usuario_id`),
-  ADD KEY `marmita_id` (`marmita_id`);
-
---
--- Índices de tabela `tb_marmitas`
---
-ALTER TABLE `tb_marmitas`
-  ADD PRIMARY KEY (`id`);
+  ADD KEY `marmita_id` (`produto_id`);
 
 --
 -- Índices de tabela `tb_pedidos`
@@ -170,7 +180,13 @@ ALTER TABLE `tb_pedidos`
 ALTER TABLE `tb_pedido_itens`
   ADD PRIMARY KEY (`id`),
   ADD KEY `pedido_id` (`pedido_id`),
-  ADD KEY `marmita_id` (`marmita_id`);
+  ADD KEY `marmita_id` (`produto_id`);
+
+--
+-- Índices de tabela `tb_produtos`
+--
+ALTER TABLE `tb_produtos`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Índices de tabela `tb_user`
@@ -186,31 +202,31 @@ ALTER TABLE `tb_user`
 -- AUTO_INCREMENT de tabela `tb_carrinho`
 --
 ALTER TABLE `tb_carrinho`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=40;
-
---
--- AUTO_INCREMENT de tabela `tb_marmitas`
---
-ALTER TABLE `tb_marmitas`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=50;
 
 --
 -- AUTO_INCREMENT de tabela `tb_pedidos`
 --
 ALTER TABLE `tb_pedidos`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
 
 --
 -- AUTO_INCREMENT de tabela `tb_pedido_itens`
 --
 ALTER TABLE `tb_pedido_itens`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
+
+--
+-- AUTO_INCREMENT de tabela `tb_produtos`
+--
+ALTER TABLE `tb_produtos`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
 
 --
 -- AUTO_INCREMENT de tabela `tb_user`
 --
 ALTER TABLE `tb_user`
-  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=30;
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=32;
 
 --
 -- Restrições para tabelas despejadas
@@ -221,14 +237,14 @@ ALTER TABLE `tb_user`
 --
 ALTER TABLE `tb_carrinho`
   ADD CONSTRAINT `tb_carrinho_ibfk_1` FOREIGN KEY (`usuario_id`) REFERENCES `tb_user` (`ID`),
-  ADD CONSTRAINT `tb_carrinho_ibfk_2` FOREIGN KEY (`marmita_id`) REFERENCES `tb_marmitas` (`ID`);
+  ADD CONSTRAINT `tb_carrinho_ibfk_2` FOREIGN KEY (`produto_id`) REFERENCES `tb_produtos` (`id`);
 
 --
 -- Restrições para tabelas `tb_pedido_itens`
 --
 ALTER TABLE `tb_pedido_itens`
   ADD CONSTRAINT `tb_pedido_itens_ibfk_1` FOREIGN KEY (`pedido_id`) REFERENCES `tb_pedidos` (`id`),
-  ADD CONSTRAINT `tb_pedido_itens_ibfk_2` FOREIGN KEY (`marmita_id`) REFERENCES `tb_marmitas` (`ID`);
+  ADD CONSTRAINT `tb_pedido_itens_ibfk_2` FOREIGN KEY (`produto_id`) REFERENCES `tb_produtos` (`id`) ON DELETE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
